dcab8ba00a072ad6252f0e2a73f12695d4e11294
Amardeep Singh <64483790+amardeep25may88@users.noreply.github.com>
2026-01-22 00:05:07 +0530

    Add files via upload

---
+++ src/main/java/com/ecommscs/EcommscsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommscsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommscsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommscs/EcommscsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommscsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
